2e8e97ada0606b4506e3c7fce6bc23e9762562a0
Pete Vander Giessen <petevg@gmail.com>
2016-08-30 15:53:46 -0400

    By default apache-bigtop-base installs Java. (#41)
    By default apache-bigtop-base installs Java.
    
    This eliminates the need for the openjdk relation, which makes
    Zookeeper more backwards compatible, and simplifies everything else.

--- tests/unit/bigtop_harness.py
+++ tests/unit/bigtop_harness.py
@@ -1,48 +1,98 @@
-from path import Path
+#
+# bigtop_harness.py -- Harness for out unit tests
+#
+
+import logging
 import mock
 import os
-import sys
 import unittest
+from path import Path
+
+LOGGER = logging.getLogger()
+LOGGER.addHandler(logging.StreamHandler())
+LOGGER.setLevel(logging.DEBUG)
 
 
-class BigtopHarness(unittest.TestCase):
-    modules_to_mock = [
-        'charms.layer.apache_bigtop_base.layer',
-        'apache_bigtop_base.hookenv'
-    ]
+class Harness(unittest.TestCase):
+    '''
+    Harness for our unit tests. Automatically patches out options, and
+    sets up hooks to make patching out hookenv.status_get easier.
 
-    def setUp(self):
-        '''Mock out many things.'''
-        self.patchers = []
-        self.mocks = {}
+    '''
 
-        for module in self.modules_to_mock:
-            patcher = mock.patch(module)
-            self.mocks[module] = patcher.start()
-            self.patchers.append(patcher)
+    #
+    # Status related stuff
+    #
 
-        # Setup status list
-        self.statuses = []
-        mock_status = self.mocks['apache_bigtop_base.hookenv']
-        mock_status.status_set = lambda a, b: self.statuses.append((a,b))
-
-    def tearDown(self):
-        for patcher in self.patchers:
-            patcher.stop()
+    @property
+    def statuses(self):
+        if not hasattr(self, '_statuses'):
+            self._statuses = []
+        return self._statuses
 
     @property
     def last_status(self):
-        '''Helper for mocked out status list.'''
+        '''
+        Helper for mocked out status list.
 
+        Returns the last status set, or a (None, None) tuple if no
+        status has been set.
+
+        '''
+        if not self.statuses:
+            return (None, None)
         return self.statuses[-1]
 
-    @classmethod
-    def tearDownClass(cls):
+    def status_set(self, status, message):
+        '''Set our mock status.'''
+
+        self.statuses.append((status, message))
+
+
+    #
+    # Misc Helpers
+    #
+
+    def log(self, msg):
         '''
-        We don't mock out some calls that write to the unit state
-        db. Clean up that file here.
+        Print a given message to STDOUT.
 
         '''
+        self._log.debug(msg)
+
+
+    #
+    # init, setUp, tearDown
+    #
+
+    def __init__(self, *args, **kwargs):
+        unittest.TestCase.__init__(self, *args, **kwargs)
+
+        self._log = LOGGER
+
+    def setUp(self):
+        '''
+        Give each test access to a clean set of mocks.
+
+        '''
+        self._patchers = []
+
+        # Patch out options
+        self._patchers.append(mock.patch(
+            'apache_bigtop_base.layer.options', create=True))
+        self._patchers.append(mock.patch(
+            'charms.layer.apache_bigtop_base.layer.options', create=True))
+
+        for patcher in self._patchers:
+            patcher.start()
+
+    def tearDown(self):
+        '''Clean up our mocks.'''
+
+        for patcher in self._patchers:
+            patcher.stop()
+
+        # Clean up state db
         cwd = os.getcwd()
         state_db = Path(cwd) / '.unit-state.db'
         if state_db.exists():
